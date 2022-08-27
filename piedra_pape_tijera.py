1f4d03383f2643e87001aea1018eccf30317a0c0
Bladimir Escobar <bladimirescobardiaz@gmail.com>
2022-08-26 19:26:48 -0500

    problema de choice arreglado

--- piedra_pape_tijera.py
+++ piedra_pape_tijera.py
@@ -2,9 +2,9 @@ import random
 
 
 def jugar():
-    usuario = input("Escoje una opcion 'PI' para piedra, 'PA' papel y 'ti' para tijera. /n").lower()
+    usuario = input("Escoje una opcion 'PI' para piedra, 'PA' papel y 'ti' para tijera. \n").lower()
 
-    computadora = random.choice("pi", "pa", "pi")
+    computadora = random.choice(["pi", "pa", "pi"])
 
     if usuario == computadora:
         return "¡Empate."
@@ -15,9 +15,12 @@ def jugar():
     return "¡Perdiste!"
 
 def gano_el_jugador(jugador, oponente):
-    if ((jugador == pi and oponente == ti)
-        or (jugador == pa and oponente == pi)
-        or (jugador == ti and oponente == pa)):
+    if ((jugador == "pi" and oponente == "ti")
+        or (jugador == "pa" and oponente == "pi")
+        or (jugador == "ti" and oponente == "pa")):
         return True
     else:
         return False
+
+
+print(jugar())
