41d9f6d8554bd6be58dc1874e06f24c2b40c04b9
Bladimir Escobar <bladimirescobardiaz@gmail.com>
2022-08-26 19:20:24 -0500

    funcion gana el jugador creada

--- piedra_pape_tijera.py
+++ piedra_pape_tijera.py
@@ -5,4 +5,19 @@ def jugar():
     usuario = input("Escoje una opcion 'PI' para piedra, 'PA' papel y 'ti' para tijera. /n").lower()
 
     computadora = random.choice("pi", "pa", "pi")
+
+    if usuario == computadora:
+        return "¡Empate."
     
+    if gano_el_jugador(usuario, computadora):
+        return "¡Ganaste."
+    
+    return "¡Perdiste!"
+
+def gano_el_jugador(jugador, oponente):
+    if ((jugador == pi and oponente == ti)
+        or (jugador == pa and oponente == pi)
+        or (jugador == ti and oponente == pa)):
+        return True
+    else:
+        return False
