7b3cf0a66b8b18cbed47614ebac0be6dc43f7189
Bladimir Escobar <bladimirescobardiaz@gmail.com>
2022-11-28 15:09:41 -0500

    actualizacion_01

--- piedra_pape_tijera.py
+++ piedra_pape_tijera.py
@@ -1,34 +1,5 @@
 import random 
 
-
-def jugar():
-    print("""
-        =========================
-        || BIENVENIDO AL JUEGO ||
-        =========================
-        """)
-
-    usuario = input("Escoje una opcion: piedra, papel o tijera. \n ==> ").lower()
-    opciones = ("piedra", "papel", "tijera")
-    computadora = random.choice(opciones)
-
-    user_win = 0
-    computer_win = 0
-    if usuario in opciones:
-
-        if usuario == computadora:
-            return f"la computadora escogio {computadora} ¡Empate."
-        elif gano_el_jugador(usuario, computadora):
-            user_win += 1
-            return f"la computadora escogio {computadora} ¡Ganaste."
-
-        elif gano_el_jugador(usuario, computadora) == False:
-            computer_win += 1
-            return f"La computadora escogio {computadora} ¡Perdiste!"
-
-    else:
-        return "opcion no valida"
-
 def gano_el_jugador(jugador, oponente):
     if ((jugador == "piedra" and oponente == "tijera")
         or (jugador == "papel" and oponente == "piedra")
@@ -38,5 +9,38 @@ def gano_el_jugador(jugador, oponente):
         return False
 
 
+def jugar():
 
+    print("""
+            =========================
+            || BIENVENIDO AL JUEGO ||
+            =========================
+            """)
+    
+    usuario = input("Escoje una opcion: piedra, papel o tijera. \n ==> ").lower()
+    opciones = ("piedra", "papel", "tijera")
+    computadora = random.choice(opciones)
+
+    user_win = 0
+    computer_win = 0
+
+    while True:
+        if usuario in opciones:
+
+            if usuario == computadora:
+                return f"la computadora escogio {computadora} ¡Empate."
+            elif gano_el_jugador(usuario, computadora):
+                user_win += 1
+                if user_win == 2:
+                    break
+                return f"la computadora escogio {computadora} ¡Ganaste."
+
+            elif gano_el_jugador(usuario, computadora) == False:
+                computer_win += 1
+                if computer_win == 2: 
+                    break
+                return f"La computadora escogio {computadora} ¡Perdiste!"
+        else:
+            return "opcion no valida"
+            
 print(jugar())
